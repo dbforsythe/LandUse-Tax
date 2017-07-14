439690c913c2fbaeaca8cd63b1cbb29ef2cc6b5c
Forsythe <dforsythe@patrick.int>
2017-07-14 11:33:49 -0500

    First commit

---
+++ assesment.py
@@ -0,0 +1,13 @@
+import sys
+sys.argv = ['SFR','125000']
+landuse = sys.argv[0]
+value = int(sys.argv[1])
+
+if landuse == "SFR":
+    rate = 0.05
+elif landuse == "MFR":
+    rate = 0.04
+else:
+    rate = 0.02
+assessment = value * rate
+print assessment
